c029352570320c5accbd40fb4a4377d2e6953aa2
Anna Presnyakova <apresnyakova88@gmail.com>
2019-07-28 19:43:52 +0200

    server set up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const flashcards = require('./routes/api/flashcards');
@@ -19,6 +20,13 @@ mongoose
     });
 
 app.use('/api/flashcards', flashcards);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT;
 app.listen(port, () => {
