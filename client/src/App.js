05f5adb3cb9a89725411aee034cb94edb27a9957
Anna Presnyakova <apresnyakova88@gmail.com>
2019-08-10 13:27:56 +0200

    - authActions: loadUser, tokenConfig - errActions

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import FlashcardsList from './components/FlashcardsList';
 import FlashcardModal from './components/FlashcardModal';
@@ -6,22 +6,29 @@ import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/authActions';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-    return (
-        <Provider store={store}>
-            <div className='App'>
-                <AppNavbar />
-                <Container>
-                    <FlashcardModal />
-                    <FlashcardsList />
-                </Container>
-            </div>
-        </Provider>
-    );
+class App extends Component {
+    componentDidMount() {
+        store.dispatch(loadUser());
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <div className='App'>
+                    <AppNavbar />
+                    <Container>
+                        <FlashcardModal />
+                        <FlashcardsList />
+                    </Container>
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
